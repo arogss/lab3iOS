33b9559eeb887ffde0a38f4cba4515b53faac010
Samuel Arogundade <samuelarogundade@Samuels-MacBook-Pro.local>
2020-01-28 18:40:49 -0500

    first commit

---
+++ Lab3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab3
+//
+//  Created by Samuel Arogundade on 2020-01-28.
+//  Copyright © 2020 Samuel Arogundade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
